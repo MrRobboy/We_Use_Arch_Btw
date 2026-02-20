5f2474fc2c9a6e2bc632f1206fc3622652894bed
MATHIS NGO <150020403+MrRobboy@users.noreply.github.com>
2026-02-20 15:06:11 +0100

    feat: setup LUKS encryption and LVM containers (including 10GB vault)

--- install.sh
+++ install.sh
@@ -48,3 +48,32 @@ partprobe $DISK
 sleep 2
 
 echo -e "\n\033[1;32m>>> Partitionnement terminé avec succès.\033[0m"
+
+echo " -> Chiffrement de la partition ${DISK}2..."
+echo -n "$PASS" | cryptsetup luksFormat --type luks2 ${DISK}2 -
+echo -n "$PASS" | cryptsetup open ${DISK}2 cryptlvm
+
+echo " -> Création du Volume Group ($VG) et des Logical Volumes..."
+pvcreate /dev/mapper/cryptlvm
+vgcreate $VG /dev/mapper/cryptlvm
+
+lvcreate -L 8G -n lv_swap $VG
+lvcreate -L 15G -n lv_vbox $VG
+lvcreate -L 5G -n lv_shared $VG
+lvcreate -L 10G -n lv_vault $VG
+lvcreate -l 100%FREE -n lv_root $VG
+
+echo " -> Formatage des partitions (ext4, fat32, swap)..."
+mkfs.fat -F32 ${DISK}1
+mkfs.ext4 /dev/$VG/lv_root
+mkfs.ext4 /dev/$VG/lv_vbox
+mkfs.ext4 /dev/$VG/lv_shared
+mkswap /dev/$VG/lv_swap
+
+echo " -> Configuration du coffre-fort chiffré (Vault)..."
+echo -n "$PASS" | cryptsetup luksFormat /dev/$VG/lv_vault -
+echo -n "$PASS" | cryptsetup open /dev/$VG/lv_vault vault_tmp
+mkfs.ext4 /dev/mapper/vault_tmp
+cryptsetup close vault_tmp
+
+echo -e "\n\033[1;32m>>> Étape 2 terminée : Le stockage est prêt et sécurisé.\033[0m"
